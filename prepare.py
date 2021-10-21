3371e700d43b9806a056cdf00dc6928e2dbe7d4a
Jeff Akins <jeffakins21@gmail.com>
2021-10-21 08:00:33 -0700

    Updates to project

--- prepare.py
+++ prepare.py
@@ -1,20 +1,25 @@
 # This is a file that will keep all of my Data Prepare Scripts
 
-# Imports
+# Imports ----------------------------------------------------------------------------------
 import numpy as np
 import pandas as pd
 import env
 import os
 
-# Functions
+# Functions --------------------------------------------------------------------------------
+
+# Function to get the Codeup curriculum logs:
 
 def get_logs():
     '''This is a function that will return all of the Codeup 
     curriculum logs and cohort information'''
-   
-    if os.path.isfile('curriculum.csv'):                        # Returns the csv version if it exists
+    
+        # Returns the csv version if it exists
+    if os.path.isfile('curriculum.csv'):                        
         df = pd.read_csv('curriculum.csv')
-    else:                                                       # Else, pulls from the sql server
+        
+        # Else, pulls from the sql server
+    else:                                                       
         sql = '''SELECT *
                  FROM logs
                  LEFT JOIN cohorts 
@@ -22,8 +27,31 @@ def get_logs():
 
         url = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/curriculum_logs'
         df = pd.read_sql(sql, url)
-        df.to_csv('curriculum.csv')                             # Creates the csv version
+
+        # Creates the csv version
+        df.to_csv('curriculum.csv')                             
 
     return df
 
 
+# Function that adds the course names to the log files:
+
+def add_course_names():
+    '''This function creates a dictionary with the Codeup course 
+    information and concatenates it with the log dataframe'''
+
+        # Creating a dataframe with the course names:
+    courses = pd.DataFrame({'id': [1, 2, 3, 4], 
+                       'course_name': ['PHP Full Stack Web Dev', 'Java Full Stack Web Dev', 'Data Science', 'Front End Web Dev'],
+                       'course_subdomain': ['php', 'java', 'ds', 'fe']})
+
+        # Getting the log files:
+    log = get_logs()
+
+        # Adding the course names to the log files:
+    log = log.merge(course, how='left', left_on='program_id', right_on='id')
+
+        # Dropping duplicate columns:
+    log = log.drop(columns=['Unnamed: 0', 'id_y', 'id_x', 'deleted_at'])
+
+    return log
