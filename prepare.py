8726cf260489e139cdbd5499c548c8419f4b0e7d
Jeff Akins <jeffakins21@gmail.com>
2021-10-21 15:33:10 -0700

    Updates to notebook and prepare file

--- prepare.py
+++ prepare.py
@@ -49,9 +49,55 @@ def add_course_names():
     log = get_logs()
 
         # Adding the course names to the log files:
-    log = log.merge(course, how='left', left_on='program_id', right_on='id')
+    log = log.merge(courses, how='left', left_on='program_id', right_on='id')
 
         # Dropping duplicate columns:
     log = log.drop(columns=['Unnamed: 0', 'id_y', 'id_x', 'deleted_at'])
+
+    return log
+
+
+# Function that will prep the data prior to exploration:
+
+def prep_logs():
+    '''This function has scripts that clean the log data from Codeups curriculum'''
+
+        # Getting the log files with course names included:
+    log = add_course_names()
+
+        # Filling the start, end, created, and update null dates with the log entry date:
+    log.start_date = log.start_date.fillna(log['date'])
+    log.end_date = log.end_date.fillna(log['date'])
+    log.created_at = log.created_at.fillna(log['date'])
+    log.updated_at = log.updated_at.fillna(log['date'])
+
+        # Combining date and time, converting to datetime, and making it the index:
+    log['timestamp'] = log.date + " " + log.time
+    log.timestamp = pd.to_datetime(log.timestamp)
+    log = log.set_index('timestamp')
+
+        # Converting all dates and time columns to datetime format:
+    log.start_date = pd.to_datetime(log.start_date)
+    log.end_date = pd.to_datetime(log.end_date)
+    log.created_at = pd.to_datetime(log.created_at)
+    log.updated_at = pd.to_datetime(log.updated_at)
+
+        # Creating an hour and weekday columns:
+    log['hour'] = log.index.hour
+    log['weekday'] = log.index.day_name()
+
+        # Dropping the original date and time columns:
+    log = log.drop(columns=['date', 'time'])
+
+        # Filling NAs with 0 for numeric columns:
+    log.cohort_id = log.cohort_id.fillna(0)
+    log.program_id = log.program_id.fillna(0)
+    log.cohort_id = log.cohort_id.astype('int64')
+    log.program_id = log.program_id.astype('int64')
+
+        # Filling NAs with 'none' for string columns:
+    column_list = ['name', 'slack', 'course_name', 'course_subdomain']
+    for col in column_list:
+        log[col] = log[col].fillna('none')
 
     return log
